637c9fc263822c3baa6fda5f545691bc3c1b5358
alphadzingina <alphaadx6@gmail.com>
2022-01-27 05:01:08 +0100

    add option to go again

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 from art import logo
 print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shift = shift % 26
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
@@ -21,6 +17,18 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text += char
   print(f"Here's the {direction}d result: {end_text}")
 
+should_end = False
+while not should_end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
 
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
